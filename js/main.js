d8f5b8ba7bd6e63e4dc9f2c855c4fe307bf2e40e
skonns <109499764+skonns@users.noreply.github.com>
2023-12-05 20:24:03 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// function openModal(imgSrc, caption) {
+//   var modal = document.getElementById('myModal');
+//   var modalImg = document.getElementById('modalImg');
+//   var captionText = document.getElementById('caption');
+
+//   modal.style.display = 'block';
+//   modalImg.src = imgSrc;
+//   captionText.innerHTML = caption;
+// }
+
+// function closeModal() {
+//   var modal = document.getElementById('myModal');
+//   modal.style.display = 'none';
+// }
+
+// // Tutup modal jika pengguna mengklik di luar gambar
+// window.onclick = function (event) {
+//   var modal = document.getElementById('myModal');
+//   if (event.target === modal) {
+//       closeModal();
+//   }
+// };
